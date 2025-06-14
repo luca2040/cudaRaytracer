e8f51a5338ddcee2d17a24a7b744f6ccdf89eaff
Luca2040 <apollo.2040.24@gmail.com>
2025-06-14 02:41:33 +0200

    added .vscode

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
-#include "iostream"
+#include <iostream>
+#include <SDL2/SDL.h>
+
+const int width = 1080;
+const int height = 1080;
 
 int main()
 {
-  std::cout << "This is a test!";
+  std::cout << "Starting the best 3D renderer ever!";
+
+  if (SDL_Init(SDL_INIT_VIDEO) < 0)
+  {
+    std::cerr << "SDL could not initialize: " << SDL_GetError() << std::endl;
+    return 1;
+  }
 
   return 0;
 }
